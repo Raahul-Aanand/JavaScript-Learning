e2b5cd311b862cbfec8976308c4a9cab6aed92c3
Rahul Anand <duffer.rdx@gmail.com>
2023-12-10 18:14:06 +0530

    throttling cmplete

--- throttling/script.js
+++ throttling/script.js
@@ -6,3 +6,26 @@
  *while debouncing executes the function after some cooling period of time 
 
  */
+
+const myInput = document.getElementById("input-event");
+
+function throttle(func, delay) {
+  let timeout = null;
+  console.log(timeout);
+  return function (...args) {
+    if (timeout === null) {
+      func.call(this, ...args);
+      timeout = setTimeout(() => {
+        console.log(timeout);
+        timeout = null;
+      }, delay);
+    }
+  };
+}
+const findElements = (e) => {
+  console.log(e.target.value);
+};
+
+const throttleFunc = throttle(findElements, 300);
+
+myInput.addEventListener("input", throttleFunc);
